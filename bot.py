aa3b474c0a34d54b107154d8ace2d77749ec7dca
lixinrui <weather2020@qq.com>
2024-08-21 12:45:18 +0800

    feat(bot): add Console and Telegram adapters, disable ONEBOT_V11Adapter registration

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ import nonebot
 # from nonebot.adapters.mirai2 import Adapter as Mirai_Adapter
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 from nonebot.adapters.satori import Adapter as Satori_Adapter
-
+from nonebot.adapters.console import Adapter as Console_Adapter
+from nonebot.adapters.telegram import Adapter as Telegram_Adapter
 # from nonebot.adapters.qq import Adapter as QQ_Adapter
 from nonebot.log import logger
 
@@ -32,10 +33,11 @@ logger.add(
 logger.info("========= 重新启动 =========")
 
 driver = nonebot.get_driver()
-driver.register_adapter(ONEBOT_V11Adapter)
+# driver.register_adapter(Console_Adapter)
 # driver.register_adapter(Satori_Adapter)
 # driver.register_adapter(Mirai_Adapter)
 # driver.register_adapter(QQ_Adapter)
+driver.register_adapter(Telegram_Adapter)
 
 
 nonebot.load_plugin("nonebot_plugin_bilichat")
